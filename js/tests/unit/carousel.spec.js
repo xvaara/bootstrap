195d013921f3ab6ab2ed1ea92dc30aaf2b03f9f8
XhmikosR <xhmikosr@gmail.com>
2021-08-26 19:23:46 +0300

    Fix `import/extensions` issues

--- js/tests/unit/carousel.spec.js
+++ js/tests/unit/carousel.spec.js
@@ -1,7 +1,7 @@
-import Carousel from '../../src/carousel'
-import EventHandler from '../../src/dom/event-handler'
-import { clearFixture, createEvent, getFixture, jQueryMock } from '../helpers/fixture'
-import { isRTL, noop } from '../../src/util/index'
+import Carousel from '../../src/carousel.js'
+import EventHandler from '../../src/dom/event-handler.js'
+import { clearFixture, createEvent, getFixture, jQueryMock } from '../helpers/fixture.js'
+import { isRTL, noop } from '../../src/util/index.js'
 
 describe('Carousel', () => {
   const { Simulator, PointerEvent } = window
